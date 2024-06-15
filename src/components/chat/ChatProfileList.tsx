f7139897e56d02628c82568b7bafda8a17b4ec17
ihteshamulhaq950 <ihteshamhaq4@gmail.com>
2024-06-15 16:18:06 +0500

    Basic Prototyping of whatsapp side profile lists and chatbox.

---
+++ src/components/chat/ChatProfileList.tsx
@@ -0,0 +1,238 @@
+import avatar from "../../../public/avatar.jpg"
+
+
+const ChatProfileList = () => {
+    return (
+        <div className="border p-6">
+            ChatProfileList
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+
+
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+            <div className="flex justify-between items-start border border-slate-700 p-2">
+                <div className="flex justify-start">
+                    <img
+                        className="w-12 h-12 rounded-full me-3"
+                        src={avatar}
+                        alt=""
+                    />
+                    <div className="flex flex-col">
+                        <p>Ihtesham</p>
+                        <small>Hi, I hope you fine.</small>
+                    </div>
+                </div>
+                <div>
+                    <small>2 days ago</small>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ChatProfileList
---
+++ src/pages/chat.tsx
@@ -0,0 +1,368 @@
+// import ChatBox from "../components/chat/ChatBox"
+// import ChatProfileList from "../components/chat/ChatProfileList"
+import avatar1 from "../../public/avatar.jpg";
+
+
+
+const ChatPage = () => {
+    return (
+        <>
+
+            <aside className="fixed top-0 left-0 lg:w-[27%] md:w-[47%] w-[100%] h-screen bg-gray-50 dark:bg-neutral-800 text-gray-900 dark:text-white">
+                <div className="h-full px-3 py-4 overflow-y-auto">
+                    <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+
+                        <li className="py-3 sm:py-4 text-gray-900 dark:text-white">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+                                        <p className="text-sm font-medium truncate">
+                                            Neil Sims
+                                        </p>
+                                        <p className="inline-flex items-center font-thin text-xs">
+                                            2 days ago
+                                        </p>
+                                    </div>
+                                    <p className="text-sm truncate">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="py-3 sm:py-4">
+                            <div className="flex items-center">
+                                <div className="flex-shrink-0">
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={avatar1} alt="Neil image" />
+                                </div>
+                                <div className="flex-1 min-w-0 ms-4">
+                                    <div className="flex justify-between">
+
+                                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                            Neil Sims
+                                        </p>
+
+                                        <p className="inline-flex items-center  font-thin
+                                        text-xs text-gray-900 dark:text-white">
+                                            2 days ago
+                                        </p>
+
+                                    </div>
+
+                                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        email@windster.com
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+
+
+            <div className="hidden lg:ml-[27%] md:ml-[47%] md:flex items-center justify-center h-screen">
+                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 flex flex-col items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 mb-4 text-white dark:text-gray-500">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+                    </svg>
+                    <h3 className="font-semibold text-white dark:text-gray-500">Whatsapp Chat App</h3>
+                    <p className="text-white dark:text-gray-500">We'd like to know your thoughts about this app</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ChatPage
+
